1f9870d1c8d817e028c65d8e57d99eab288e3945
Alex Gartner <git@agartner.com>
2023-12-10 17:58:57 -0800

    day10 part 2 minimal work

--- day10/solve.py
+++ day10/solve.py
@@ -1,11 +1,21 @@
 import typing as T
 import sys
-from collections import namedtuple
+import copy
+from functools import reduce
+import os
+import subprocess
+import time
 
 DiagramT = T.List[T.List[str]]
-Point = namedtuple("Point", "x y")
+
+
+class Point(T.NamedTuple):
+    x: int
+    y: int
+
 
 StartSym = "S"
+
 
 def load(path: str) -> DiagramT:
     with open(path) as f:
@@ -13,23 +23,85 @@ def load(path: str) -> DiagramT:
 
     return [[*line.strip()] for line in lines]
 
+
 def find_start_pos(diagram: DiagramT) -> Point:
     for x, line in enumerate(diagram):
         for y, sym in enumerate(line):
             if sym == StartSym:
-                return Point(x,y)
-            
-def calc_cycle_midpoint_distance(diagram: DiagramT, origin_pt: Point, start_pt: Point = None) -> int:
+                return Point(x, y)
+
+
+def print_diagram(diagram: DiagramT):
+    for line in diagram:
+        print("".join(line))
+
+
+def clean_diagram(diagram: DiagramT, loop_pts: T.Set[Point]) -> DiagramT:
+    """
+    remove irrelevent symbols from the diagram
+    """
+    diagram = copy.copy(diagram)
+    for x, line in enumerate(diagram):
+        for y, sym in enumerate(line):
+            if sym == ".":
+                continue
+            p = Point(x, y)
+            if p in loop_pts:
+                continue
+            line[y] = " "
+    return diagram
+
+
+def line_has_sym(line: T.List[str], start_idx: int, step: int) -> bool:
+    idx = start_idx
+    while idx < len(line) and idx > 0:
+        sym = line[idx]
+        if sym != " " and sym != ".":
+            return True
+        idx += step
+    return False
+
+
+def col_has_sym(diagram: DiagramT, origin: Point, step: int) -> bool:
+    idx = origin.x
+    while idx < len(diagram) and idx > 0:
+        sym = diagram[idx][origin.y]
+        if sym != " " and sym != ".":
+            return True
+        idx += step
+    return False
+
+
+def prune_obvious_outside(diagram: DiagramT) -> DiagramT:
+    diagram = copy.copy(diagram)
+    for x, line in enumerate(diagram):
+        for y, sym in enumerate(line):
+            if sym != ".":
+                continue
+            left = line_has_sym(line, y, -1)
+            right = line_has_sym(line, y, 1)
+            up = col_has_sym(diagram, Point(x, y), -1)
+            down = col_has_sym(diagram, Point(x, y), 1)
+            if left and right and up and down:
+                continue
+            line[y] = " "
+
+    return diagram
+
+
+def calc_loop(diagram: DiagramT, origin_pt: Point, start_pt: Point = None) -> T.Tuple[int, int]:
     # this is a hack, in both the example and input you can just go one to the right to start
     p_pt = origin_pt
-    c_pt = Point(origin_pt.x, origin_pt.y+1) if start_pt is None else start_pt
+    c_pt = Point(origin_pt.x, origin_pt.y + 1) if start_pt is None else start_pt
     current_sym = None
     ctr = 1
+    loop_pts = {origin_pt}
     while True:
+        loop_pts.add(c_pt)
         current_sym = diagram[c_pt.x][c_pt.y]
         if current_sym == StartSym:
             break
-        print(current_sym, p_pt, c_pt, ctr)
+        # print(current_sym, p_pt, c_pt, ctr)
         next_p_pt = c_pt
         match current_sym:
             case "J":
@@ -49,12 +121,21 @@ def calc_cycle_midpoint_distance(diagram: DiagramT, origin_pt: Point, start_pt: 
 
         p_pt = next_p_pt
         ctr += 1
-    
-    print(ctr)
-    return ctr // 2
+
+    loop_midpoint_distance = ctr // 2
+
+    cdiagram = clean_diagram(diagram, loop_pts)
+    print_diagram(cdiagram)
+    pcdiagram = prune_obvious_outside(diagram)
+    print_diagram(pcdiagram)
+
+    raw_period_ctr = sum(row.count(".") for row in pcdiagram)
+
+    return loop_midpoint_distance, raw_period_ctr
+
 
 if __name__ == "__main__":
     diagram = load(sys.argv[1])
     start_pt = find_start_pos(diagram)
-    midpoint_distance = calc_cycle_midpoint_distance(diagram, start_pt)
-    print(f"midpoint_distance: {midpoint_distance}")
+    midpoint_distance, raw_period_ctr = calc_loop(diagram, start_pt)
+    print(f"midpoint_distance: {midpoint_distance} raw_period_ctr: {raw_period_ctr}")
--- day10/test_solve.py
+++ day10/test_solve.py
@@ -1,28 +1,43 @@
 from solve import *
 import pytest
 
-@pytest.mark.parametrize("path, expected_start_pos", [
-    ["example1.txt", Point(1,1)],
-    ["example2.txt", Point(2,0)],
-    ["example3.txt", Point(1,1)],
-    ["example4.txt", Point(1,1)],
-    ["input.txt", Point(92,43)],
-])
+
+@pytest.mark.parametrize(
+    "path, expected_start_pos",
+    [
+        ["example1.txt", Point(1, 1)],
+        ["example2.txt", Point(2, 0)],
+        ["example3.txt", Point(1, 1)],
+        ["example4.txt", Point(1, 1)],
+        ["example5.txt", Point(4, 12)],
+        ["input.txt", Point(92, 43)],
+    ],
+)
 def test_load(path, expected_start_pos):
     diagram = load(path)
     start_pt = find_start_pos(diagram)
     assert start_pt == expected_start_pos
 
-@pytest.mark.parametrize("path, expected_midpoint_distance,alt_start_point", [
-    ["example1.txt", 4, Point(1,2)],
-    ["example2.txt", 8, Point(3,0)],
-    ["example3.txt", 23, Point(1,2)],
-    ["example4.txt", 22, Point(1,2)],
-    ["input.txt", 7107, Point(92,42)],
-])
+
+@pytest.mark.parametrize(
+    "path, expected_midpoint_distance,alt_start_point",
+    [
+        ["example1.txt", 4, Point(1, 2)],
+        ["example2.txt", 8, Point(3, 0)],
+        ["example3.txt", 23, Point(1, 2)],
+        ["example4.txt", 22, Point(1, 2)],
+        ["example5.txt", 70, None],
+        ["input.txt", 7107, Point(92, 42)],
+    ],
+)
 def test_cycle_midpoint_distance(path, expected_midpoint_distance, alt_start_point):
     diagram = load(path)
     start_pt = find_start_pos(diagram)
 
-    assert calc_cycle_midpoint_distance(diagram, start_pt) == expected_midpoint_distance
-    assert calc_cycle_midpoint_distance(diagram, start_pt, start_pt=alt_start_point) == expected_midpoint_distance
+    assert calc_loop(diagram, start_pt) == expected_midpoint_distance
+    assert calc_loop(diagram, start_pt, start_pt=alt_start_point) == expected_midpoint_distance
+
+
+def test_point_in_set():
+    s = {Point(1, 2)}
+    assert Point(1, 2) in s
