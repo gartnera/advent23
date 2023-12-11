cb65241b3456adecfbbf37d492437d294f5a299b
Alex Gartner <git@agartner.com>
2023-12-10 20:31:54 -0800

    day10 still not working :(

--- day10/solve.py
+++ day10/solve.py
@@ -1,10 +1,7 @@
 import typing as T
 import sys
 import copy
-from functools import reduce
-import os
-import subprocess
-import time
+from print_color import print
 
 DiagramT = T.List[T.List[str]]
 
@@ -31,22 +28,41 @@ def find_start_pos(diagram: DiagramT) -> Point:
                 return Point(x, y)
 
 
-def print_diagram(diagram: DiagramT):
-    for line in diagram:
-        print("".join(line))
+def print_color_fixed_width(num: int):
+    color = "blue" if (num // 10 % 2 == 0) else "green"
+    print(num % 10, color=color, end="")
 
 
-def clean_diagram(diagram: DiagramT, loop_pts: T.Set[Point]) -> DiagramT:
+def print_diagram(
+    diagram: DiagramT, red_pts: T.Set[Point] = None, green_pts: T.Set[Point] = None, number=True
+):
+    if number:
+        print(" ", end="")
+    for y in range(len(diagram[0])):
+        if number:
+            print_color_fixed_width(y)
+    if number:
+        print()
+    for x, line in enumerate(diagram):
+        if number:
+            print_color_fixed_width(x)
+        for y, sym in enumerate(line):
+            point = Point(x, y)
+            color = "red" if red_pts and point in red_pts else ""
+            color = "green" if green_pts and point in green_pts else color
+            print(sym, end="", color=color)
+        print()
+
+
+def clean_diagram(diagram: DiagramT, good_pts: T.Set[Point]) -> DiagramT:
     """
     remove irrelevent symbols from the diagram
     """
     diagram = copy.copy(diagram)
     for x, line in enumerate(diagram):
-        for y, sym in enumerate(line):
-            if sym == ".":
-                continue
+        for y, _ in enumerate(line):
             p = Point(x, y)
-            if p in loop_pts:
+            if p in good_pts:
                 continue
             line[y] = " "
     return diagram
@@ -89,6 +105,24 @@ def prune_obvious_outside(diagram: DiagramT) -> DiagramT:
     return diagram
 
 
+def calc_inner_point(prev: Point, current: Point) -> Point:
+    return Point(current.x + (current.y - prev.y), current.y + (prev.x - current.x))
+
+
+def dilate_inner_points(inner_pts: T.Set[Point], loop_pts: T.Set[Point]):
+    did_dialate = False
+    for inner_pt in inner_pts.copy():
+        for i in range(3):
+            for j in range(3):
+                dc_point = Point(inner_pt.x + i - 1, inner_pt.y + j - 1)
+                if dc_point == inner_pt or dc_point in inner_pts or dc_point in loop_pts:
+                    continue
+                inner_pts.add(dc_point)
+                did_dialate = True
+    if did_dialate:
+        dilate_inner_points(inner_pts, loop_pts)
+
+
 def calc_loop(diagram: DiagramT, origin_pt: Point, start_pt: Point = None) -> T.Tuple[int, int]:
     # this is a hack, in both the example and input you can just go one to the right to start
     p_pt = origin_pt
@@ -96,12 +130,18 @@ def calc_loop(diagram: DiagramT, origin_pt: Point, start_pt: Point = None) -> T.
     current_sym = None
     ctr = 1
     loop_pts = {origin_pt}
+    raw_inner_pts = set()
     while True:
         loop_pts.add(c_pt)
         current_sym = diagram[c_pt.x][c_pt.y]
         if current_sym == StartSym:
             break
         # print(current_sym, p_pt, c_pt, ctr)
+        inner_pt = calc_inner_point(p_pt, c_pt)
+        # print(f"inner_pt: {inner_pt}")
+        if diagram[inner_pt.x][inner_pt.y]:
+            raw_inner_pts.add(inner_pt)
+        # print_diagram(diagram, red_pts={inner_pt}, green_pts={c_pt})
         next_p_pt = c_pt
         match current_sym:
             case "J":
@@ -124,18 +164,19 @@ def calc_loop(diagram: DiagramT, origin_pt: Point, start_pt: Point = None) -> T.
 
     loop_midpoint_distance = ctr // 2
 
-    cdiagram = clean_diagram(diagram, loop_pts)
-    print_diagram(cdiagram)
-    pcdiagram = prune_obvious_outside(diagram)
-    print_diagram(pcdiagram)
+    cdiagram = diagram
+    # cdiagram = prune_obvious_outside(diagram)
+    inner_pts = raw_inner_pts - loop_pts
+    print_diagram(cdiagram, red_pts=inner_pts)
+    dilate_inner_points(inner_pts, loop_pts)
+    cdiagram = clean_diagram(diagram, loop_pts.union(inner_pts))
+    print_diagram(cdiagram, red_pts=inner_pts)
 
-    raw_period_ctr = sum(row.count(".") for row in pcdiagram)
-
-    return loop_midpoint_distance, raw_period_ctr
+    return loop_midpoint_distance, len(inner_pts)
 
 
 if __name__ == "__main__":
     diagram = load(sys.argv[1])
     start_pt = find_start_pos(diagram)
-    midpoint_distance, raw_period_ctr = calc_loop(diagram, start_pt)
-    print(f"midpoint_distance: {midpoint_distance} raw_period_ctr: {raw_period_ctr}")
+    midpoint_distance, inner_ctr = calc_loop(diagram, start_pt)
+    print(f"midpoint_distance: {midpoint_distance} inner_ctr: {inner_ctr}")
--- day10/test_solve.py
+++ day10/test_solve.py
@@ -19,25 +19,32 @@ def test_load(path, expected_start_pos):
     assert start_pt == expected_start_pos
 
 
-@pytest.mark.parametrize(
-    "path, expected_midpoint_distance,alt_start_point",
-    [
-        ["example1.txt", 4, Point(1, 2)],
-        ["example2.txt", 8, Point(3, 0)],
-        ["example3.txt", 23, Point(1, 2)],
-        ["example4.txt", 22, Point(1, 2)],
-        ["example5.txt", 70, None],
-        ["input.txt", 7107, Point(92, 42)],
-    ],
-)
-def test_cycle_midpoint_distance(path, expected_midpoint_distance, alt_start_point):
-    diagram = load(path)
-    start_pt = find_start_pos(diagram)
-
-    assert calc_loop(diagram, start_pt) == expected_midpoint_distance
-    assert calc_loop(diagram, start_pt, start_pt=alt_start_point) == expected_midpoint_distance
-
-
 def test_point_in_set():
     s = {Point(1, 2)}
     assert Point(1, 2) in s
+
+
+@pytest.mark.parametrize(
+    "path, expected_midpoint_distance,expected_inside_ctr,alt_start_point",
+    [
+        ["example1.txt", 4, None, None],
+        ["example2.txt", 8, None, None],
+        ["example3.txt", 23, 4, None],
+        ["example4.txt", 22, 4, None],
+        ["example5.txt", 70, 8, Point(5, 12)],
+        ["example6.txt", 80, 10, Point(1, 4)],
+        # can't get correct answer for part 2 :( for the real input
+        # 274 is not correct :(
+        ["input.txt", 7107, 274, None],
+    ],
+)
+def test_cycle_midpoint_distance(
+    path, expected_midpoint_distance, expected_inside_ctr, alt_start_point
+):
+    diagram = load(path)
+    start_pt = find_start_pos(diagram)
+
+    midpoint, inside_ctr = calc_loop(diagram, start_pt, start_pt=alt_start_point)
+    assert midpoint == expected_midpoint_distance
+    if expected_inside_ctr:
+        assert inside_ctr == expected_inside_ctr
