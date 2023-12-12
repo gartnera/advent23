a12ff04ebdc0a65c6bc78e0b334872b011a6e5c1
Alex Gartner <git@agartner.com>
2023-12-11 20:54:21 -0800

    day11 part1 working

--- day11/solve.py
+++ day11/solve.py
@@ -5,6 +5,9 @@ import sys
 import networkx as nx
 import matplotlib.pyplot as plt
 from print_color import print
+from copy import copy
+from itertools import combinations
+from tqdm import tqdm
 
 
 def point_to_name(x: int, y: int) -> str:
@@ -45,11 +48,28 @@ class Lines(list):
     def point_has_galaxy(self, point: Point) -> bool:
         return self.row_has_galaxy(point.x) or self.col_has_galaxy(point.y)
 
+    def print(self):
+        for line in self:
+            print(line)
+
 
 class Universe(T.NamedTuple):
     galaxy_id_to_point: IdPointMapT
     graph: nx.Graph
     lines: Lines
+    max_galaxy_id: int
+
+    def distance_between_points(self, a: Point, b: Point) -> int:
+        path = nx.shortest_path(self.graph, a.name(), b.name())
+        return len(path) - 1
+
+    def distance_between_all_points(self) -> int:
+        res = 0
+        for lhand, rhand in tqdm(list(combinations(range(1, self.max_galaxy_id), 2))):
+            lhand_pt = self.galaxy_id_to_point[lhand]
+            rhand_pt = self.galaxy_id_to_point[rhand]
+            res += self.distance_between_points(lhand_pt, rhand_pt)
+        return res
 
 
 def show_graph(graph: nx.Graph):
@@ -57,20 +77,21 @@ def show_graph(graph: nx.Graph):
 
     esmall = [(u, v) for (u, v, d) in graph.edges(data=True)]
 
-    pos = nx.spring_layout(graph)  # positions for all nodes
+    pos = nx.spectral_layout(graph)  # positions for all nodes
 
     # nodes
     nx.draw_networkx_nodes(graph, pos, node_size=700)
 
     # edges
-    nx.draw_networkx_edges(graph, pos, edgelist=elarge, width=6)
+    nx.draw_networkx_edges(graph, pos, edgelist=elarge, width=2)
     nx.draw_networkx_edges(
-        graph, pos, edgelist=esmall, width=6, alpha=0.5, edge_color="b", style="dashed"
+        graph, pos, edgelist=esmall, width=2, alpha=0.5, edge_color="b", style="dashed"
     )
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, "weight"))
+    # nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, "weight"))
     nx.draw_networkx_labels(graph, pos, font_size=20, font_family="sans-serif")
 
     plt.axis("off")
+    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
     plt.show()
 
 
@@ -82,54 +103,64 @@ HVNeighborsOffsets = [
 ]
 
 
-def load(path: str, diagonals: bool = False) -> Universe:
+def load(path: str) -> Universe:
     with open(path) as f:
         raw_lines = f.readlines()
 
     # convert to numeric
-    galaxy_id = 1
+    max_galaxy_id = 1
     lines: Lines = Lines()
-    galaxy_id_to_point: IdPointMapT = {}
-    for x, raw_line in enumerate(raw_lines):
+    for _, raw_line in enumerate(raw_lines):
         line = []
-        for y, sym in enumerate(raw_line.strip()):
+        for _, sym in enumerate(raw_line.strip()):
             if sym == "#":
-                line.append(galaxy_id)
-                galaxy_id_to_point[galaxy_id] = Point(x, y)
-                galaxy_id += 1
+                line.append(max_galaxy_id)
+                max_galaxy_id += 1
             else:
                 line.append(0)
         lines.append(line)
 
+    expanded_lines = Lines(*copy(lines))
+    # reverse iteration to perserve insert indexing
+    for i, row in reversed(tuple(enumerate(lines))):
+        if lines.row_has_galaxy(i):
+            continue
+        new_row = [0 for _ in range(len(row))]
+        expanded_lines.insert(i, new_row)
+
+    for j in range(len(lines[0]) - 1, -1, -1):
+        if lines.col_has_galaxy(j):
+            continue
+
+        for line in expanded_lines:
+            line.insert(j, 0)
+
+    galaxy_id_to_point: IdPointMapT = {}
+    for x, line in enumerate(expanded_lines):
+        for y, sym in enumerate(line):
+            if sym == 0:
+                continue
+            galaxy_id_to_point[sym] = Point(x, y)
+
     g = nx.Graph()
 
-    for i, line in enumerate(lines):
+    for i, line in enumerate(expanded_lines):
         for j, sym in enumerate(line):
             current = Point(i, j)
             for ii, jj in HVNeighborsOffsets:
                 x = i + ii
                 y = j + jj
                 target = Point(x, y)
-                if x < 0 or x > len(lines) - 1 or y < 0 or y > len(line) - 1:
+                if x < 0 or x > len(expanded_lines) - 1 or y < 0 or y > len(line) - 1:
                     continue
                 if current == target:
                     continue
-                weight = (
-                    1 if lines.point_has_galaxy(current) and lines.point_has_galaxy(target) else 2
-                )
-                g.add_edge(current.name(), target.name(), weight=weight)
+                g.add_edge(current.name(), target.name())
 
-    return Universe(galaxy_id_to_point, g, lines)
-
-
-def distance_between_points(graph: nx.Graph, a: Point, b: Point) -> int:
-    path = nx.shortest_path(graph, a.name(), b.name(), weight="weight")
-    print(path)
-    weights = [graph[path[i]][path[i + 1]]["weight"] for i in range(len(path) - 1)]
-    print(weights)
-    return sum(weights)
+    return Universe(galaxy_id_to_point, g, expanded_lines, max_galaxy_id)
 
 
 if __name__ == "__main__":
     universe = load(sys.argv[1])
-    show_graph(universe.graph)
+    res = universe.distance_between_all_points()
+    print(res)
--- day11/test_solve.py
+++ day11/test_solve.py
@@ -6,43 +6,29 @@ from solve import *
 @pytest.mark.parametrize(
     "path,shape",
     [
-        ["example1.txt", ()],
+        ["myexample1.txt", Point(4, 4)],
+        ["myexample2.txt", Point(4, 4)],
+        ["example1.txt", Point(13, 12)],
     ],
 )
-def test_load(path, shape):
-    load(path)
-
-
-@pytest.mark.parametrize(
-    "path,point,row_has_galaxy,col_has_galaxy",
-    [
-        ["example1.txt", Point(7, 2), False, False],
-        ["myexample2.txt", Point(0, 1), True, False],
-        ["myexample2.txt", Point(1, 0), False, True],
-        ["myexample2.txt", Point(1, 2), False, True],
-        ["myexample2.txt", Point(1, 1), False, False],
-    ],
-)
-def test_point_has_galaxy(path, point, row_has_galaxy, col_has_galaxy):
+def test_load(path, shape: Point):
     universe = load(path)
-    assert universe.lines.row_has_galaxy(point.x) == row_has_galaxy
-    assert universe.lines.col_has_galaxy(point.y) == col_has_galaxy
-    assert universe.lines.point_has_galaxy(point) == row_has_galaxy or col_has_galaxy
+    assert len(universe.lines) == shape.y
+    assert len(universe.lines[0]) == shape.x
 
 
 @pytest.mark.parametrize(
     "path,src_id,dst_id,length",
     [
-        ["myexample2.txt", 1, 2, 4],
+        ["myexample2.txt", 1, 2, 6],
         ["example1.txt", 5, 9, 9],
+        ["example1.txt", 1, 7, 15],
+        ["example1.txt", 3, 6, 17],
     ],
 )
-def test_(path, src_id, dst_id, length):
+def test_distance_between_points(path, src_id, dst_id, length):
     universe = load(path)
 
-    assert (
-        distance_between_points(
-            universe.graph, universe.galaxy_id_to_point[src_id], universe.galaxy_id_to_point[dst_id]
-        )
-        == length
-    )
+    src_point = universe.galaxy_id_to_point[src_id]
+    dst_point = universe.galaxy_id_to_point[dst_id]
+    assert universe.distance_between_points(src_point, dst_point) == length
